35021ed55a5b076e4766df894d639ced9ac90862
June <165972652+qjsarah@users.noreply.github.com>
2025-02-18 07:55:45 +0800

    Create mainscaffold.dart

---
+++ midterm/mainscaffold.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'screens/HomePage.dart';
+
+void main() {
+  runApp( const MaterialApp(title: 'UI design',
+  debugShowCheckedModeBanner: false,
+  home: HomePage(), 
+    ),
+  );
+}
